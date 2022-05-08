e5fe1f6e29adf4221280a9ead86a7f76ac98ab98
Sebastian Martínez <55563384+vSebas@users.noreply.github.com>
2022-05-08 02:36:13 -0500

    deleted unnecessary files

--- ApiMySql/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const userController = require('./user.controller');
-const router = express.Router();
-
-router.post('/user', userController.login);
-
-module.exports = router;
